2c45b9f9d5aad66cd543b10aa4e47f70ac9ae457
Steve Sokasits <steve.sokasits@gmail.com>
2018-10-28 16:57:47 -0500

    latest changes

--- src/main/java/com/doorCreekCafe/controller/Admin.java
+++ src/main/java/com/doorCreekCafe/controller/Admin.java
@@ -11,7 +11,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.Principal;
 
 
 /**
@@ -26,19 +28,21 @@ import java.io.IOException;
 
 public class Admin extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
         GenericDao userDao = new GenericDao(User.class);
 
-        req.setAttribute("users", userDao.getAll());
+        //req.setAttribute("users", userDao.getAll());
         //if (req.getParameter("submit").equals("Display All Resources")) {
-        //    req.setAttribute("users", userDao.getAllUsers());
+            req.setAttribute("users", userDao.getByPropertyEqual("userName", req.getParameter("j_username")));
         //}
         RequestDispatcher dispatcher = req.getRequestDispatcher("/content/adminDashboard.jsp");
         dispatcher.forward(req, resp);
     }
+
+
 }
 
 
